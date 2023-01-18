043c52b8a36f5fa66ff6c9124a833249c13bf013
christian-the-brewer <brewer.christiang@gmail.com>
2023-01-18 14:28:44 -0500

    adds main and readme

---
+++ main.py
@@ -0,0 +1,11 @@
+#Author: Christian Brewer
+from turtle import Turtle, Screen
+
+#gameboard
+screen = Screen()
+screen.setup(width=800,height=600)
+screen.bgcolor("black")
+screen.title("Pong")
+screen.tracer(0)
+
+screen.exitonclick()
