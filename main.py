d8c4431206d47ca74c862e09afbeefefd9039cad
christian-the-brewer <brewer.christiang@gmail.com>
2023-01-18 15:26:27 -0500

    creates paddle class that inherits Turtle and gives it move up and down

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 #Author: Christian Brewer
 from turtle import Turtle, Screen
+from paddle import Paddle
+import time
 
 #gameboard
 screen = Screen()
@@ -7,5 +9,19 @@ screen.setup(width=800,height=600)
 screen.bgcolor("black")
 screen.title("Pong")
 screen.tracer(0)
+
+l_paddle = Paddle((-375, 0))
+r_paddle = Paddle((375, 0))
+
+screen.listen()
+screen.onkey(r_paddle.move_up, "Up")
+screen.onkey(r_paddle.move_down, "Down")
+screen.onkey(l_paddle.move_up, "o")
+screen.onkey(l_paddle.move_down, "q")
+
+game = True
+while game:
+    screen.update()
+    time.sleep(0.08)
 
 screen.exitonclick()
